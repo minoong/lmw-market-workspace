0c8ee696411d9a99baa2206883fe3fa305f9ab0e
lmw <alsdndjdrktl@gmail.com>
2022-05-11 18:28:41 +0900

    feat : eslint and prettier 코드 컨벤션 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+ env: {
+  browser: true,
+  es2021: true,
+ },
+ extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended'],
+ parser: '@typescript-eslint/parser',
+ parserOptions: {
+  ecmaFeatures: {
+   jsx: true,
+  },
+  ecmaVersion: 'latest',
+  sourceType: 'module',
+ },
+ plugins: ['react', '@typescript-eslint'],
+ rules: {
+  'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+  'no-shadow': 'off',
+ },
+}
